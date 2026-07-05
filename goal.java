fce797728243cd8ce137b2d3670bb19ac8368f44
kondapallisruthi332-source <kondapallisruthi332@gmail.com>
2026-07-05 14:42:28 +0530

    Add files via upload

---
+++ goal.java
@@ -0,0 +1,5 @@
+public class goal {
+  public static void main(String args[]){
+    System.out.println("i am sruthi .Iam currently studying btech CSE and my goal is to become a software engeneering. ");
+  }
+}
---
+++ names.java
@@ -0,0 +1,8 @@
+public class names {
+  public static void main(String args[])
+  {
+    System.out.println("my name is sruthi");
+    System.out.println("my college name is miracle educational society group of institutions");
+    System.out.println("my goal is to become a software ");
+  }
+}
